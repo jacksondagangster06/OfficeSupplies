3bd4d50ccf15d2053b227e68a6c3ca23c0cccac0
Reza Mohammed <rmohammed25@students.polytechnic.org>
2024-02-01 10:10:59 -0800

    water dis

---
+++ WaterDispenser.java
@@ -0,0 +1,18 @@
+public class WaterDispenser
+{
+    private  int canister;
+
+    public WaterDispenser()
+    {
+        canister = 10000;
+    }
+
+    public getAmount()
+    {
+        return canister;
+    }
+    
+    public fillBottle())
+    {
+        if (canister.)
+    }
