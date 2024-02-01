2c5bb8021ab51c07ebe695bb09f7cc01a07ea941
Jackson Brathwaite <jbrathwaite25@students.polytechnic.org>
2024-02-01 10:41:32 -0800

    Finished WaterDispenser.java

--- WaterDispenser.java
+++ WaterDispenser.java
@@ -1,18 +1,66 @@
 public class WaterDispenser
 {
     private  int canister;
-
+    private boolean leak;
+    public final double DRIP = 1.5;
     public WaterDispenser()
     {
         canister = 10000;
+        leak = false;
     }
 
-    public getAmount()
+    public int getAmount()
     {
         return canister;
     }
-    
-    public fillBottle())
+    /**
+     * fillBottle method fills a 500ml water bottle
+     **/
+    public void fillBottle()
     {
-        if (canister.)
+        int rand = (int) (Math.random() * 3);
+        if(rand == 0)
+        {
+            leak = true;
+        }
+
+        if (canister >= 500 && leak == false)
+        {
+            canister -= 500;
+        }
+        else if(canister >= 500 && leak == true)
+        {
+            canister -= 500 * DRIP;
+        }
+        else
+        {
+            System.out.println("No more water, refill dispenser");
+        }
     }
+
+    public void fillCup()
+    {
+        int rand = (int) (Math.random() * 3);
+        if(rand == 0)
+        {
+            leak = true;
+        }
+        if (canister >= 200)
+        {
+            canister -= 200;
+        }
+        else if(canister >= 200 && leak == true)
+        {
+            canister -= 200 * DRIP;
+        }
+        else
+        {
+            System.out.println("No more water, refill dispenser");
+        }
+    }
+
+    public void refill()
+    {
+        canister = 1000;
+    }
+}
