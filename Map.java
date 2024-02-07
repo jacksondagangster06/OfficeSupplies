73d21037ef227c302c5e0dc2551358cfc54c8e98
Jackson Brathwaite <jbrathwaite25@students.polytechnic.org>
2024-02-06 18:09:16 -0800

    Updated Map.java

--- Map.java
+++ Map.java
@@ -13,11 +13,27 @@ public class Map
         states.add(new State("California","Sacremento"));
         states.add(new State("New York","Albany"));
         states.add(new State("Florida","Tallahassee"));
+        states.add(new State("Texas", "Austin"));
+        states.add(new State("Colorado", "Denver"));
+        states.add(new State("Maine", "Augusta"));
+        states.add(new State("Montana", "Helena"));
+        states.add(new State("Nebraska", "Lincoln"));
         
     }
 
     public void spin()
     {
-        
+        rand = (int)(Math.random() * 8);
     }
+
+    public String getState()
+    {
+        return states.get(rand).getName();
+    }
+
+    public String getCapital()
+    {
+        return states.get(rand).getCapital();
+    }
+
 }
